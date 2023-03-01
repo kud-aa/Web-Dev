09fe3be317240a8739a4866c037c36f312f64184
kud-aa <akudaybergenov@miem.hse.ru>
2023-03-01 22:59:10 +0600

    added likes

--- Lab5/angular/src/app/product-item/product-item.component.ts
+++ Lab5/angular/src/app/product-item/product-item.component.ts
@@ -14,6 +14,15 @@ export class ProductItemComponent {
     this.remove.emit(item);
   }
 
+  toggleLike(item: any) {
+    if (item.liked) {
+      item.likes = 0;
+    } else {
+      item.likes = 1;
+    }
+    item.liked = !item.liked;
+  }
+
   share(id:number) {
     const item = this.items[id - 1];
     const url = 'https://t.me/share/url?url=' + encodeURIComponent(item.url) + '&text=' + encodeURIComponent(item.name);
