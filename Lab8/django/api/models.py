b9f3acc5cbf5c418dce99ec8654c93cb1877cbf2
kud-aa <akudaybergenov@miem.hse.ru>
2023-04-05 14:42:01 +0600

    added test product

--- Lab8/django/api/models.py
+++ Lab8/django/api/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 
 class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
+    items = models.ManyToManyField('Product', related_name='categories', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    url = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
+    rating = models.FloatField()
     count = models.IntegerField()
     is_active = models.BooleanField()
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
