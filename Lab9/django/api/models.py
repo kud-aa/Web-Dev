6faca9ec7d04cbcdf52d6b2f6c19f7cb74679211
kud-aa <akudaybergenov@miem.hse.ru>
2023-04-12 17:26:05 +0600

    added admin for test data

--- Lab9/django/api/models.py
+++ Lab9/django/api/models.py
@@ -6,8 +6,14 @@ class Company(models.Model):
     city = models.CharField(max_length=255)
     address = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Vacancy(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     salary = models.FloatField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
